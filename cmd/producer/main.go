b7220c5cfe956887fdd6081aa6b882997a70815e
thsc47 <thsc47@gmail.com>
2024-01-03 19:33:42 -0300

    feat: add `deliveryChan`

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"log"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("mensagem", "teste", producer, nil)
+	Publish("mensagem", "teste", producer, nil, deliveryChan)
+	e := <-deliveryChan
+	msg := e.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("Erro ao enviar")
+	} else {
+		fmt.Println("Mensagem enviada:", msg.TopicPartition)
+	}
 	producer.Flush(1000)
 }
 
@@ -23,13 +32,13 @@ func NewKafkaProducer() *kafka.Producer {
 	return producer
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
